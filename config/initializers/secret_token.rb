20f5e49d051b6ef0995a619f780ca51a2ca356ff
Piotr Olma <perdo.olma@gmail.com>
2012-11-05 18:44:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpaHello::Application.config.secret_token = 'a38fb4ce8c30a2f9c6e7d2efb80a163b9c472ce7ac5381b1f0d1b16d3b0ffde25147702961e6165ee14fd91d225daf5d1135b941dab6c6485e5bc37e1b2bcbd3'
